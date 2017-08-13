1b5503cda334b0e431f02a08e904f2795a8bc0c9
aryankit <ankitaryakissu2@gmail.com>
2017-08-13 17:37:44 +0530

    initial commit

---
+++ js/multiplejsfile.js
@@ -0,0 +1,18 @@
+// JavaScript
+var multiple = new Multiple({
+  selector: '.item',
+  background: 'url(warrior.jpg)'//'linear-gradient(#273463, #8B4256)'
+});
+
+// JavaScript
+var multiple = new Multiple({
+  selector: '.item2',
+  background: 'url(scene.jpg)'//'linear-gradient(#273463, #8B4256)'
+});
+
+
+//main backgroung gradiant
+var multiple = new Multiple({
+  selector: '.item3',
+  background: 'linear-gradient(#6DBDD6, #CBE32D)'
+});
---
+++ js/typedapp.js
@@ -0,0 +1,8 @@
+  var typed2 = new Typed('#typed2', {
+    strings: ['Hii...', 'you are at official website of Live Lire', 'oh this website?', 'Its just result of our developers tryin out new stuff', 'hope youre having great times :)'],
+    typeSpeed: 35,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true,
+  });
+    
